0fa1f47224b3ac711af4091149637220a78a6763
Muskaan Rajput <muskaanrajput14@gmail.com>
2024-02-19 05:13:26 +0530

    added check.cpp

---
+++ check.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"just checking something";
+    // https://www.datacamp.com/tutorial/git-push-pull --- refer this site
+    return 0;
+}
