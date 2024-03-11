b5efaebe9fb87f54fc70ea1bf386579cf8932b2c
Muskaan <muskaanrajput14@gmail.com>
2024-03-12 03:33:34 +0530

    insertion sort added

---
+++ Step2. Sorting/InsertionSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int arr[], int n)
+{
+    for(int i =0; i<n-1; i++)
+    {
+        int j =i;
+        while(j>0 && arr[j-1] > arr[j])
+        {
+            swap(arr[j], arr[j-1]);
+            j--;
+        }
+    }
+}
+
+// tc- O(N) iff sorted array is the input. Also iff the input is sorted array, the loop never runs
+int main()
+{
+    int n =5;
+    int arr[]={8,1,4,0,3};
+    InsertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<' ';
+    }
+    
+    return 0;
+}
