2087e71d95263aed7eae22df13bbd135728114a7
Muskaan <muskaanrajput14@gmail.com>
2024-03-20 16:29:28 +0530

    Meduim Problems started

---
+++ Step3. Arrays/Medium/1.Two(2)-sum.cpp
@@ -0,0 +1,15 @@
+// https://www.codingninjas.com/studio/problems/reading_6845742?leftPanelTabValue=PROBLEM
+
+
+string read(int n, vector<int> book, int target)
+{
+    // Write your code here.
+    for(int i =0; i<n; i++)
+    {
+        for(int j =i+1; j<n; j++)
+        {
+            if(book[i] + book[j] == target) return "YES";
+        }
+    }
+    return "NO";
+}
