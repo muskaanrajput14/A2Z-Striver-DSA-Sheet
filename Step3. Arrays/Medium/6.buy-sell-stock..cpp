6850e3fcc8602e6272081942924a0460dad0fb14
Muskaan <muskaanrajput14@gmail.com>
2024-03-22 01:59:43 +0530

    added 6.buy-sell-stock.cpp

---
+++ Step3. Arrays/Medium/6.buy-sell-stock..cpp
@@ -0,0 +1,15 @@
+// https://www.codingninjas.com/studio/problems/best-time-to-buy-and-sell-stock_6194560?leftPanelTabValue=PROBLEM
+
+
+
+int bestTimeToBuyAndSellStock(vector<int>&prices) {
+    // Write your code here.
+    int mini = prices[0];
+    int maxprofit  = 0;
+    for(int i=0; i<prices.size(); i++){
+        int cost = prices[i] - mini;
+        maxprofit = max(maxprofit,cost);
+        mini = min(mini,prices[i]);
+    }
+    return maxprofit;
+}
